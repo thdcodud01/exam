eefd5ce90e4d533e728e4038c053fa181162b67a
thdcodud01 <thdcodud01@gmail.com>
2023-07-07 09:22:15 +0900

    .

---
+++ src/test/java/com/ll/exams/ExamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.exams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
